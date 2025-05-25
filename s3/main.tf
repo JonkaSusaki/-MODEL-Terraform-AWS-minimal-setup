845a72e1931b81f01098ef11ce29e3d97e384b9d
joaosusaki <jrabalem@gmail.com>
2025-05-25 20:32:46 -0300

    Dynamic s3 buckets

--- s3/main.tf
+++ s3/main.tf
@@ -3,6 +3,29 @@ variable "env" {
   default = "dev"
 }
 
+variable "bucket_names" {
+  type    = list(string)
+  default = ["bucket1", "bucket2", "bucket3"]
+
+}
+
+resource "aws_s3_bucket" "bucket" {
+  for_each      = toset(var.bucket_names)
+  bucket        = "${var.env}-bucket-${each.key}"
+  force_destroy = true
+}
+
+# Creation of public access block
+resource "aws_s3_bucket_public_access_block" "public_block" {
+  for_each = aws_s3_bucket.bucket
+  bucket   = each.value.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 data "aws_iam_policy_document" "allow_public_access" {
   statement {
     effect = "Allow"
@@ -11,27 +34,13 @@ data "aws_iam_policy_document" "allow_public_access" {
       identifiers = ["*"]
     }
     actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.name.arn}/*"]
+    resources = [for bucket in aws_s3_bucket.bucket : "${bucket.arn}/*"]
   }
 }
 
-resource "aws_s3_bucket" "name" {
-  bucket        = "${var.env}-bucket-bakko-1"
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_policy" "allow_public_access" {
-  bucket = aws_s3_bucket.name.id
-  policy = data.aws_iam_policy_document.allow_public_access.json
-
-  depends_on = [ aws_s3_bucket_public_access_block.name ]
-}
-
-resource "aws_s3_bucket_public_access_block" "name" {
-  bucket = aws_s3_bucket.name.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+resource "aws_s3_bucket_policy" "policies" {
+  for_each     = aws_s3_bucket.bucket
+  bucket       = each.value.id
+  policy       = data.aws_iam_policy_document.allow_public_access.json
+  depends_on = [aws_s3_bucket_public_access_block.public_block]
 }
